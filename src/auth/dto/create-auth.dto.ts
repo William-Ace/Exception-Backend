06c976ee03020b6671a70b8b29ceeeac7fc833af
William-Ace <WilliamLimWork@outlook.com>
2023-04-11 20:39:46 +0200

    Swagger added, vercel added

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,10 +1,16 @@
 // DTO : Data Transfer Object
 import { IsNotEmpty } from 'class-validator';
+import { AutoMap } from '@automapper/classes';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAuthDto {
+  @ApiProperty()
+  @AutoMap()
   @IsNotEmpty()
   readonly email: string;
 
+  @ApiProperty()
+  @AutoMap()
   @IsNotEmpty()
   readonly password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Exception example')
+    .setDescription('The exception API description')
+    .setVersion('1.0')
+    .addTag('exception')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('auth/register', app, document);
   await app.listen(3000);
 }
 bootstrap();
